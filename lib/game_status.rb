c87670821de6635f9f632bc93174d1b4abdbf88e
Anthony Fields <janthonyfields@gmail.com>
2017-09-20 02:17:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
     if board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3]
-      won = board.select(win_combo)
+      won = board.detect { |x| x == win_combo } 
       binding.pry
     else
       won = false
