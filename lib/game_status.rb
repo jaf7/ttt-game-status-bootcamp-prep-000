7becea54562319366845989c89932d06df843369
Anthony Fields <janthonyfields@gmail.com>
2017-09-20 03:57:14 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,11 +23,11 @@ def won?(board)
     win_index_3 = win_combo[2]
     if position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3)
       board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] ? result = win_combo : result = false
-      binding.pry # result => [0,1,2]
+      # binding.pry # result => [0,1,2]
     else
       result = false
     end
-    binding.pry # result => [0,1,2]
+    # binding.pry # result => [0,1,2]
   end
   binding.pry # result => NameError: undefined local variable or method `blah' for main:Object
   result
