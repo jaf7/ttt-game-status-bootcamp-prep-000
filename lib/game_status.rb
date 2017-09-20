b3f33a7c2f1c4bb665914cab0c966820f1137ac9
Anthony Fields <janthonyfields@gmail.com>
2017-09-20 04:23:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,11 +18,12 @@ WIN_COMBINATIONS = [
 def won?(board)
   win = nil # ensure that scope of "result" variable will encompass the do-end block passed to #each
   WIN_COMBINATIONS.each do |win_combo|
-    win_index_1 = win_combo[0]
+    position_1 = win_combo[0]
     win_index_2 = win_combo[1]
     win_index_3 = win_combo[2]
-    if position_taken?(board, win_index_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3)
-      board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] ? return win_combo : false
+    if position_taken?(board, position_1) && position_taken?(board, win_index_2) && position_taken?(board, win_index_3)
+      if board[position_1] == board[win_index_2] && board[win_index_2] == board[win_index_3]
+      # board[position_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] ? return win_combo : false
       # binding.pry # result => [0,1,2]
     else
       false
